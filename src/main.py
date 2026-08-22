fba1f4d68073ce08bc2c14faecbe9ef11edb110e
Dogukan Cihanbeyoglu <54885883+dogukancihanbeyoglu@users.noreply.github.com>
2026-08-22 14:38:24 +0300

    security: disable debug mode by default in secondary entrypoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+import os
+
 from app import app
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    debug_enabled = os.environ.get("FLASK_DEBUG", "").lower() in {"1", "true", "yes"}
+    app.run(host="0.0.0.0", port=5000, debug=debug_enabled)
